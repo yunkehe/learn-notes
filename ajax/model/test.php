49880b5c6ed39ceaaf87db0c33eebe691b92482c
何珂 <yunkehe@163.com>
2017-02-05 18:01:54 +0800

    angular

---
+++ ajax/model/test.php
@@ -0,0 +1,6 @@
+<?php 	
+	$name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
+	$computedString = "Hi, " . $name;
+	$array = ['userName' => $name, 'computedString' => $computedString];
+	echo json_encode($array);
+?>
---
+++ libraries/angular/js/beginner.js
@@ -0,0 +1,6 @@
+var app = angular.module('myApp', []);
+app.controller('HelloAngular', function($scope) {
+    $scope.greeting = {
+    	text: 'Hello'
+    }
+});
