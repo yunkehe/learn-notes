7fd289561457d552ec88252a5d24980e2a2a92cd
yunkehe <yunkehe@163.com>
2017-02-12 17:44:38 +0800

    del file

--- libraries/angular/project-binary-data-bind/CSS1.js
+++
@@ -1,9 +0,0 @@
-var myCSSModule = angular.module('MyCSSModule', []);
-myCSSModule.controller('CSSCtrl', ['$scope',
-    function($scope) {
-        $scope.color = "red";
-        $scope.setGreen = function() {
-            $scope.color = "green";
-        }
-    }
-])
