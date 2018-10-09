4b00ba7b5b4d3c2dfec40656b850855049e5f21e
yunkehe <yunkehe@163.com>
2018-10-09 17:37:09 +0800

    sublime extension setting

---
+++ windows/cmder/user-profile.sh
@@ -0,0 +1,7 @@
+# use this file to run your own startup commands for msys2 bash'
+
+# To add a new vendor to the path, do something like:
+# export PATH=${CMDER_ROOT}/vendor/whatever:${PATH}
+alias loginvps='ssh yunkehe@45.32.66.58'
+alias cdcoding='cd /d/CODING'
+alias cdwork='cd /d/shengxue/projects'
